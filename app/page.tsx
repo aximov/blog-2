58ae735eb9a74ed60e017ed1e2e9aa2124af2d3f
Nakayama Daichi <daichi.nakayama@legalscape.co.jp>
2025-08-02 16:49:49 +0900

    fix: 不要な連絡先セクションを削除

--- app/page.tsx
+++ app/page.tsx
@@ -23,27 +23,6 @@ export default function Page() {
         </Link>
       </div>
       <div className="my-8">
-        <h2 className="mb-4 text-xl font-medium tracking-tighter">Contact</h2>
-        <ul className="space-y-2">
-          <li>
-            <Link
-              href="https://twitter.com/aximovich"
-              className="underline hover:no-underline"
-            >
-              Twitter
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://github.com/aximov"
-              className="underline hover:no-underline"
-            >
-              GitHub
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="my-8">
         <h2 className="mb-4 text-xl font-medium tracking-tighter">
           Blog Posts
         </h2>
